c9f38511132878b9c2dd5b7e320878a734d882e7
JosiahFu <josiahfu@gmail.com>
2023-09-07 19:04:28 -0700

    routes comments

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,20 +1,27 @@
 import express, { Express } from 'express';
 import { Db } from 'mongodb';
 
+// See https://expressjs.com/en/guide/routing.html for more documentation
+
 function setupRoutes(app: Express, db: Db) {
+    // Interpret requests as JSON
     app.use(express.json());
+    // Serve files from client/dist (the React app)
     app.use(express.static('client/dist'));
 
+    // Get the collection from the database
     const collection = db.collection<{ value: number }>('documents');
 
+    // POSTs to /api/count should add a database entry
     app.post<{}, {}, { value: number }>('/api/count', async (req, res) => {
         await collection.insertOne({ value: req.body.value });
         console.log(`Recieved count entry for ${req.body.value}`);
-        res.end('Data recieved successfully');
+        res.end();
     });
 
+    // GET to /api/count should return all the data
     app.get('/api/count', async (req, res) => {
-        res.end(JSON.stringify(await collection.find({}).toArray()));
+        res.json(await collection.find({}).toArray());
     });
 }
 
